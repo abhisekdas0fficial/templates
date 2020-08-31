528bf3a631bd283d0f54bd2ac8da9fc17bf2d512
abhisekdas0fficial <abhisekdasatwork@gmail.com>
2020-08-31 20:45:22 +0530

    updated js

--- element hover effect/main.js
+++ element hover effect/main.js
@@ -1,6 +1,6 @@
 const icard = document.querySelector(".icard");
 icard.addEventListener("mousemove", e => {
-    icard.setAttribute("style", "transform: rotateX("+(-(e.layerY-150)*5/150)+"deg) rotateY("+((e.layerX-225)*5/225)+"deg);");
+    icard.setAttribute("style", "transform: rotateX("+(-(e.layerY-150)*10/150)+"deg) rotateY("+((e.layerX-225)*7/225)+"deg);");
 });
 icard.addEventListener("mouseout", e => {
     icard.setAttribute("style", "transform: rotateX(0deg) rotateY(0deg);");
