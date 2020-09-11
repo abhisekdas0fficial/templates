e77c7471954f81654291f7543579e350b3a9ca68
abhisekdas0fficial <abhisekdasatwork@gmail.com>
2020-09-11 23:59:31 +0530

    edit

--- text hover effect/main.js
+++ text hover effect/main.js
@@ -1,7 +1,27 @@
-const cursor = document.querySelector(".cursor");
-document.addEventListener("mousemove", e => {
-    cursor.setAttribute("style", "top: " + e.pageY + "px; left: " + e.pageX + "px;")
+const cursor1 = document.querySelector(".cursor-1");
+const cursor2 = document.querySelector(".cursor-2");
+const header = document.querySelector("header");
+const logo = document.querySelector("#logo");
+const section = document.querySelector("section");
+var handler;
+
+header.addEventListener("mousemove", handler = e => {
+    cursor1.setAttribute("style", "top: " + (e.pageY - 8) + "px; left: " + (e.pageX - 8) + "px; display: inline-block;");
 });
-document.addEventListener("mouseout", e => {
-    cursor.style.display = "none";
+header.addEventListener("mouseout", e => {
+    cursor1.style.display = "none";
+});
+
+section.addEventListener("mousemove", e => {
+    cursor2.setAttribute("style", "top: " + (e.pageY - 8) + "px; left: " + (e.pageX - 8) + "px; display: inline-block;");
+});
+section.addEventListener("mouseout", e => {
+    cursor2.style.display = "none";
+});
+
+logo.addEventListener("mouseover", e => {
+    header.removeEventListener("mousemove", handler);
+});
+logo.addEventListener("mouseout", e => {
+    header.addEventListener("mousemove", handler);
 });
